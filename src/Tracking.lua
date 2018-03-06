d14c5c1912818c5280ec9fd081367db728d23ca2
Garret Jennings <gjennings@me.com>
2018-03-05 16:28:53 -0800

    Move event registration and unregistration to their own functions.

--- src/Tracking.lua
+++ src/Tracking.lua
@@ -8,7 +8,37 @@
 
 local currentStack = 0
 
-function GFC.OnStackChanged(eventCode, changeType, effectSlot, effectName,
+function GFC.RegisterEvents()
+
+    -- Events for each skill morph
+    -- Separate namespaces for each are required as
+    -- duplicate filters against the same namespace
+    -- overwrite the previously set filter.
+    --
+    -- These filter the EVENT_EFFECT_CHANGED event to
+    -- hit the callback *only* when the three specific
+    -- ability IDs change and avoid the need to conditionally
+    -- exclude all skills we are not interested in.
+
+    for morph, morphValue in pairs(GFC.ABILITIES) do
+        for skillType, skillValue in pairs(morphValue) do
+            GFC:Trace(2, "Registering: " .. morph .. " (" .. skillType .. ": " .. skillValue.ID .. ")")
+            EVENT_MANAGER:RegisterForEvent(skillValue.NAME, EVENT_EFFECT_CHANGED, function(...) GFC.OnEffectChanged(...) end)
+            EVENT_MANAGER:AddFilterForEvent(skillValue.NAME, EVENT_EFFECT_CHANGED, REGISTER_FILTER_ABILITY_ID, skillValue.ID)
+            EVENT_MANAGER:AddFilterForEvent(skillValue.NAME, EVENT_EFFECT_CHANGED, REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE, COMBAT_UNIT_TYPE_PLAYER)
+        end
+    end
+end
+
+function GFC.UnregisterEvents()
+    for morph, morphValue in pairs(GFC.ABILITIES) do
+        for skillType, skillValue in pairs(morphValue) do
+            GFC:Trace(2, "Unregistering: " .. morph .. " (" .. skillType .. ": " .. skillValue.ID .. ")")
+            EVENT_MANAGER:UnregisterForEvent(skillValue.NAME, EVENT_EFFECT_CHANGED)
+        end
+    end
+end
+
         unitTag, startTimeSec, endTimeSec, stackCount, iconName, buffType,
         effectType, abilityType, statusEffectType, unitName, unitId,
         effectAbilityId)
