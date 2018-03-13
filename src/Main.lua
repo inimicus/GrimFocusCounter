56c249d3e1edd01e7d82154d22cb730b630c401f
Garret Jennings <gjennings@me.com>
2018-03-12 20:52:45 -0700

    Bump version to v1.2.0

--- src/Main.lua
+++ src/Main.lua
@@ -10,7 +10,7 @@
 -- -----------------------------------------------------------------------------
 GFC             = {}
 GFC.name        = "GrimFocusCounter"
-GFC.version     = "1.1.2"
+GFC.version     = "1.2.0"
 GFC.dbVersion   = 1
 GFC.slash       = "/gfc"
 GFC.prefix      = "[GFC] "
